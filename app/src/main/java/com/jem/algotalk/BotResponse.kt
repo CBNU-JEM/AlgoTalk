4423cd60243dd128092cfd995533e0af53566a42
huey.j <huey.j@kakaocorp.com>
2021-03-14 21:29:46 +0900

    ALGO-1 rasa 연결

---
+++ app/src/main/java/com/jem/algotalk/BotResponse.kt
@@ -0,0 +1,9 @@
+package com.jem.algotalk
+
+class BotResponse(
+    var recipient_id: String,
+    var text: String,
+    var buttons: List<Buttons>
+) {
+    inner class Buttons(var payload: String, var title: String)
+}
